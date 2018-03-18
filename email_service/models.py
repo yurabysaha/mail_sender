99eefaab132ddc3086c68c35984df22d39e5fa91
shyrakens <sasha.yevtushenko@aiesec.net>
2018-03-18 22:01:25 +0200

    Email model2

--- email_service/models.py
+++ email_service/models.py
@@ -2,22 +2,11 @@ from django.db import models
 
 
 class Email(models.Model):
-    first_name = models.CharField(max_length=30, min_lenght=3, blank=True)
-    last_name = models.CharField(max_length=30,min_lenght=3, blank=True)
-    email = models.EmailField(max_length=1200,min_lenght=3, blank=True)
+    first_name = models.CharField(max_length=30, min_lenght=3)
+    last_name = models.CharField(max_length=30, blank=True)
+    email = models.EmailField(min_lenght=3)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
 
-
-
-    def email_message(self):
-        return self.email
-
-    def email_owner(self):
-        return self.first_name, self.last_name
-
-
-    def email_date(self):
-        return self.created_date, self.updated_date
 
 
