91786ea191a839cce066e79a234ae04a37710f92
yurabysaha <yurabysaha@gmail.com>
2018-03-18 22:08:15 +0200

    Cosmetics fix

--- email_service/models.py
+++ email_service/models.py
@@ -2,11 +2,8 @@ from django.db import models
 
 
 class Email(models.Model):
-    first_name = models.CharField(max_length=30, min_lenght=3)
+    first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30, blank=True)
-    email = models.EmailField(min_lenght=3)
+    email = models.EmailField()
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
-
-
-
