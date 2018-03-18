1b3cc44b63363150119bac497b038a6460f416df
Tetiana Bysaha <tanyabysaha@gmail.com>
2018-03-18 11:35:38 +0200

    Added the function to parse XLSX files

--- email_service/parsing_xlsx.py
+++ email_service/parsing_xlsx.py
@@ -1,11 +1,11 @@
 import pandas as pd
 from parsing_CSV_function import parsing_csv
 
+def parsing_xlsx(file_path):
+    data_xls = pd.read_excel(file_path, index_col=None)
+    data_xls.to_csv('created.csv', encoding='utf-8',index=False)
 
-data_xls = pd.read_excel(file_path, index_col=None)
-data_xls.to_csv('created.csv', encoding='utf-8',index=False)
-
-print(parsing_csv('created.csv'))
+    return parsing_csv('created.csv')
 
 
 
