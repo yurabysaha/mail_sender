1fad6acd06be2fe8e1e5be9a913c0f72ad796645
yurabysaha <yurabysaha@gmail.com>
2018-03-18 21:08:58 +0200

    Cosmetics fix

--- job_board/forms.py
+++ job_board/forms.py
@@ -2,9 +2,7 @@ from django import forms
 from .models import Job
 
 
-
 class JobForm(forms.ModelForm):
-
 
     class Meta:
         model = Job
--- job_board/views.py
+++ job_board/views.py
@@ -1,10 +1,9 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.utils import timezone
 
 from job_board.forms import JobForm
 from .models import Job
-from .models import Job
-
 
 
 def job_list(request):
@@ -17,21 +16,15 @@ def job_delete(request, job_id):
     job.delete()
     return redirect("/jobs/")
 
-def job_create(request):
 
+def job_create(request):
     if request.method == "POST":
         form = JobForm(request.POST)
         if form.is_valid():
-            job = form.save()
-            job.created_at = timezone.now()
-            job.user = Job.objects.get(user = user_id)
-            job.save()
-            return redirect('jobs', pk=job.pk)
+            form = form.save(commit=False)
+            form.user_id = User.objects.first().id
+            form.save()
+            return redirect('jobs')
     else:
         form = JobForm()
     return render(request, 'job_list/job_create.html', {'form': form})
-
-
-
-
-
