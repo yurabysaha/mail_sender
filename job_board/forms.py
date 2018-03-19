8248c050fe252f84cf7a74614040084dc2acfd36
Tetiana Bysaha <tanyabysaha@gmail.com>
2018-03-19 21:31:13 +0200

    Greated bootsrap view for forms on create and edit

--- job_board/forms.py
+++ job_board/forms.py
@@ -7,3 +7,9 @@ class JobForm(forms.ModelForm):
     class Meta:
         model = Job
         fields = ('title', 'email_text',)
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'email_text':forms.Textarea(attrs={'class': 'form-control'})
+        }
+
+
