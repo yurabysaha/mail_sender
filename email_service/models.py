5a3181da50dd30a7762e15e20b263c00de04cdd1
yurabysaha <yurabysaha@gmail.com>
2018-03-19 23:35:42 +0200

    Add Job to Email

--- email_service/models.py
+++ email_service/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
+from job_board.models import Job
+
 
 class Email(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30, blank=True)
     email = models.EmailField()
+    job = models.ForeignKey(Job, related_name='emails', on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
