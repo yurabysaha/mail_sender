34854858050c5c9f6a78947ab67db36247223094
Tetiana Bysaha <tanyabysaha@gmail.com>
2018-03-19 16:24:05 +0200

    Create a job_create function and button. Have a problem with user, not sure whether it is because we don't have a proper model yet or not.

--- job_board/urls.py
+++ job_board/urls.py
@@ -6,6 +6,7 @@ from job_board import views
 urlpatterns = [
     path('', views.job_list, name = "jobs"),
     path('<int:job_id>/delete', views.job_delete),
-    path('create', views.job_create, name = "create job")
+    path('create', views.job_create, name = "create job"),
+    path('<int:job_id>/edit', views.job_edit)
 ]
 
