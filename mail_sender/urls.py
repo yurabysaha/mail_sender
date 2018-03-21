2b9f058bcd8c3a87850f04a3d403245b940d430f
yurabysaha <yurabysaha@gmail.com>
2018-03-21 10:04:18 +0200

    Add debug tool

--- mail_sender/urls.py
+++ mail_sender/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import include
-
+from django.conf.urls import include, url
 
 urlpatterns = [
     path('', include("landing_page.urls")),
@@ -24,3 +24,9 @@ urlpatterns = [
     path('user/', include('user_profile.urls')),
     path('jobs/', include("job_board.urls")),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
