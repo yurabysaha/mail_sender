50c9cbdb23962ae3ef76e0db89a97f395fd2d125
Tetiana Bysaha <tanyabysaha@gmail.com>
2018-03-21 23:30:52 +0200

    Greated bootstrap view for register page

--- email_service/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.utils import timezone
-from email_service.models import Email
-
-
-def email_list(request):
-    emails = Email.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
-    return render(request, 'job_list/job_details.html', {"emails": emails})
