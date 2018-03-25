b21f4847bbb2bfa25d3a3aeafc604c677396b061
Yura <yurabysaha@gmail.com>
2018-03-25 14:52:38 +0300

    Update local_settings.py

--- mail_sender/local_settings.py
+++ mail_sender/local_settings.py
@@ -4,7 +4,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'mail_sender',
-        'USER': 'posrgres',
+        'USER': 'postgres',
         'PASSWORD': 'yura123',
         'HOST': 'localhost',
         'PORT': '5432',
