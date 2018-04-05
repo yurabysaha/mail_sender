040fd0b5c98431d0c1345e495a222b38f0ef5277
grigorevskiy <grigorevskiiy@gmail.com>
2018-04-05 15:59:01 +0300

    Added upload file and adding email from uploaded file

--- email_service/parsing_xlsx.py
+++ email_service/parsing_xlsx.py
@@ -2,8 +2,8 @@ import pandas as pd
 from .parsing_CSV_function import parsing_csv
 
 
-def parsing_xlsx(file_path, title):
-    data_xls = pd.read_excel(title, index_col=None)
-    data_xls.to_csv('/Users/viktorgrigorevskiy/Desktop/projects/mail_sender/media/{}.csv'.format(file_name), encoding='utf-8',index=False)
+def parsing_xlsx(file_path):
+    data_xls = pd.read_excel(file_path, index_col=None)
+    data_xls.to_csv('created.csv', encoding='utf-8',index=False)
 
-    return parsing_csv('media/{}.csv'.format(title))
+    return parsing_csv('created.csv')
--- email_service/views.py
+++ email_service/views.py
@@ -7,29 +7,31 @@ from email_service.models import Email
 from job_board.models import Job
 
 
-
-# def upload_file(request, job_id):
-#     if request.method == 'POST':
-#         form = UploadFileForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             parsing_xlsx(request.FILES['file'])
-#             return render(request, "job_list/job_details.html")
-#     else:
-#         form = UploadFileForm()
-#
-#     return render(request, "job_list/upload.html", {'form': form})
+def handle_uploaded_file(f, title):
+    with open('/Users/viktorgrigorevskiy/Desktop/projects/mail_sender/media/{}.csv'.format(title), 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
 
 
+def upload_file(request, job_id):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'], request.POST['title'])
 
-def add_email(request, job_id):
-    data = csv.reader(open("/Users/viktorgrigorevskiy/Desktop/projects/mail_sender/created.csv"), delimiter=",")
-    job = get_object_or_404(Job, id=job_id)
+            data = csv.reader(open("/Users/viktorgrigorevskiy/Desktop/projects/mail_sender/created.csv"), delimiter=",")
+            job = get_object_or_404(Job, id=job_id)
 
-    for row in data:
-        email = Email(first_name=row[0],
-                      last_name=row[1],
-                      email=row[2],
-                      job=job)
+            for row in data:
+                email = Email(first_name=row[0],
+                              last_name=row[1],
+                              email=row[2],
+                              job=job)
+                email.save()
 
-        email.save()
-    return render(request, "job_list/job_details.html")
+            return render(request, "job_list/job_details.html", {'job':job})
+
+    else:
+        form = UploadFileForm()
+
+    return render(request, "job_list/upload.html", {'form': form})
