a3c482fdc76bfc89c3e2d73050f2bb9cc780276a
grigorevskiy <grigorevskiiy@gmail.com>
2018-04-05 17:26:46 +0300

    28 - refactoring

--- email_service/views.py
+++ email_service/views.py
@@ -1,6 +1,5 @@
 from .forms import UploadFileForm
 from django.shortcuts import render, redirect, get_object_or_404
-from job_board.views import add_email
 import csv
 from email_service.models import Email
 from job_board.models import Job
@@ -9,14 +8,13 @@ from .parsing_xlsx import parsing_xlsx
 
 def upload_file(request, job_id):
     if request.method == 'POST':
+        job = get_object_or_404(Job, id=job_id)
+
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             parsing_xlsx(request.FILES['file'], request.POST['title'])
-            file_name = request.POST['title']
 
-            data = csv.reader(open('./media/{}.csv'.format(file_name)), delimiter=",")
-            job = get_object_or_404(Job, id=job_id)
-
+            data = csv.reader(open('./media/{}.csv'.format(request.POST['title'])), delimiter=",")
             for row in data:
                 email = Email(first_name=row[0],
                               last_name=row[1],
