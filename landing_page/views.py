6369afe7792e56dc5c503a0cba2fe2784fee86dc
Yura <yurabysaha@gmail.com>
2018-04-11 17:35:41 +0300

    Update views.py

--- landing_page/views.py
+++ landing_page/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render, redirect
 
 
 def index_page(request):
+    if request.user.is_authenticated:
+        return redirect("jobs")
     return render(request, 'index.html', {})
